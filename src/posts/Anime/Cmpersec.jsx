53e9e4b30c5293b7049b99c8c46503f8083d0c37
nilotpaul <nilotpaul.nandi@gmail.com>
2023-04-03 23:38:35 +0530

    fixes

--- src/posts/Anime/Cmpersec.jsx
+++ src/posts/Anime/Cmpersec.jsx
@@ -5,17 +5,17 @@ import '/src/components/styles/Posts.css'
 const Cmpersecond = () => {
   return (
     <div className='parent'>
-    <CImage class='thumb' fluid src="/public/Cmpersec/1.jpg" />
-    br
-    <CImage class='thumb' fluid src="/public/Cmpersec/2.jpg" />
-    br
-    <CImage class='thumb' fluid src="/public/Cmpersec/3.jpg" />
-    br
-    <CImage class='thumb' fluid src="/public/Cmpersec/4.jpg" />
-    br
-    <CImage class='thumb' fluid src="/public/Cmpersec/5.jpg" />
-    br
-    <CImage class='thumb' fluid src="/public/Cmpersec/6.jpg" />
+    <CImage class='thumb' fluid src="Cmpersec/1.jpg" />
+
+    <CImage class='thumb' fluid src="Cmpersec/2.jpg" />
+
+    <CImage class='thumb' fluid src="Cmpersec/3.jpg" />
+
+    <CImage class='thumb' fluid src="Cmpersec/4.jpg" />
+
+    <CImage class='thumb' fluid src="Cmpersec/5.jpg" />
+
+    <CImage class='thumb' fluid src="Cmpersec/6.jpg" />
 
 </div>
 
